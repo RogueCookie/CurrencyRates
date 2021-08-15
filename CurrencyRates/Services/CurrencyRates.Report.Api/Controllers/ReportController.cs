1a63536ed8bf2267b28b55cb396d52582d9430f7
Valeriia Vaganova <vvvagan3@stream.ru>
2021-08-15 10:23:54 +0200

    ReportApi clean up unused part

---
+++ CurrencyRates/Services/CurrencyRates.Report.Api/Controllers/ReportController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace CurrencyRates.Report.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    [Produces("application/json", "application/xml")]
+    public class ReportController : ControllerBase
+    {
+        private readonly ILogger<ReportController> _logger;
+
+        public ReportController(ILogger<ReportController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Dratuti
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public ActionResult<string> Get()
+        {
+            return "kotiki";
+        }
+    }
+}
--- CurrencyRates/Services/CurrencyRates.Report.Api/Startup.cs
+++ CurrencyRates/Services/CurrencyRates.Report.Api/Startup.cs
@@ -1,19 +1,12 @@
+using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using CurrencyRates.Report.Api.Extensions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace CurrencyRates.Report.Api
 {
@@ -29,17 +22,31 @@ namespace CurrencyRates.Report.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.RegisterSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             app.RegisterSwaggerUi();
 
             app.UseHttpsRedirection();
+
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    context.Response.ContentType = "text/html";
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                    var exceptionHandlerFeature =
+                        context.Features.Get<IExceptionHandlerFeature>();
+                    logger.LogError(new EventId(), exceptionHandlerFeature.Error, exceptionHandlerFeature.Error.Message);
+
+                    await context.Response.WriteAsync("Something wrong");
+                });
+            });
 
             app.UseRouting();
 

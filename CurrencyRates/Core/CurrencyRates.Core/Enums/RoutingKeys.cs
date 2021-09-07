7ea1d5c9dfb8da9400093536f8fd724f96f9e150
Valeriia Vaganova <vvvagan3@stream.ru>
2021-09-07 20:01:00 +0200

    Move routing keys in Core

---
+++ CurrencyRates/Core/CurrencyRates.Core/Enums/RoutingKeys.cs
@@ -0,0 +1,13 @@
+﻿namespace CurrencyRates.Core.Enums
+{
+    /// <summary>
+    /// Contains all routing keys for connection to rabbit
+    /// </summary>
+    public enum RoutingKeys
+    {
+        /// <summary>
+        /// For registration new job in scheduler
+        /// </summary>
+        AddNewJob = 1
+    }
+}

4c603d6f7a5c5af8ae660a72339d731e1de8021e
Valeriia Vaganova <vvvagan3@stream.ru>
2021-09-11 09:11:19 +0200

    Prepared proncipal model for sharing between services

---
+++ CurrencyRates/Core/CurrencyRates.Core/Enums/TypeOfCurrency.cs
@@ -0,0 +1,173 @@
+﻿namespace CurrencyRates.Core.Enums
+{
+    /// <summary>
+    /// Kinds of available currency shortcut. Can be extended at any time
+    /// </summary>
+    public enum TypeOfCurrency
+    {
+        /// <summary>
+        /// Australia dollar
+        /// </summary>
+        AUD = 1,
+
+        /// <summary>
+        /// Brazil real
+        /// </summary>
+        BRL = 2,
+
+        /// <summary>
+        /// Bulgaria lev
+        /// </summary>
+        BGN = 3,
+
+        /// <summary>
+        /// Canada dollar
+        /// </summary>
+        CAD = 4,
+
+        /// <summary>
+        /// China renminbi
+        /// </summary>
+        CNY = 5,
+
+        /// <summary>
+        /// Croatia kuna
+        /// </summary>
+        HRK = 6,
+
+        /// <summary>
+        /// Denmark krone
+        /// </summary>
+        DKK = 7,
+
+        /// <summary>
+        /// EMU euro
+        /// </summary>
+        EUR = 8,
+
+        /// <summary>
+        /// Hongkong dollar
+        /// </summary>
+        HKD = 9,
+
+        /// <summary>
+        /// Hungary forint
+        /// </summary>
+        HUF = 10,
+
+        /// <summary>
+        /// Iceland krona
+        /// </summary>
+        ISK = 11,
+
+        /// <summary>
+        /// IMF SDR
+        /// </summary>
+        XDR = 12,
+
+        /// <summary>
+        /// India rupee
+        /// </summary>
+        INR = 13,
+
+        /// <summary>
+        /// Indonesia rupiah
+        /// </summary>
+        IDR = 14,
+
+        /// <summary>
+        /// Israel new shekel
+        /// </summary>
+        ILS = 15,
+
+        /// <summary>
+        /// Japan yen
+        /// </summary>
+        JPY = 16,
+
+        /// <summary>
+        /// Malaysia ringgit
+        /// </summary>
+        MYR = 17,
+
+        /// <summary>
+        /// Mexico peso
+        /// </summary>
+        MXN = 18,
+
+        /// <summary>
+        /// New Zealand dollar
+        /// </summary>
+        NZD = 19,
+
+        /// <summary>
+        /// Norway krone
+        /// </summary>
+        NOK = 20,
+
+        /// <summary>
+        /// Philippines peso
+        /// </summary>
+        PHP = 21,
+
+        /// <summary>
+        /// Poland zloty
+        /// </summary>
+        PLN = 22,
+
+        /// <summary>
+        /// Romania leu
+        /// </summary>
+        RON = 23,
+
+        /// <summary>
+        /// Russia rouble
+        /// </summary>
+        RUB = 24,
+
+        /// <summary>
+        /// Singapore dollar
+        /// </summary>
+        SGD = 25,
+
+        /// <summary>
+        /// South Africa rand
+        /// </summary>
+        ZAR = 26,
+
+        /// <summary>
+        /// South Korea won
+        /// </summary>
+        KRW = 27,
+
+        /// <summary>
+        /// Sweden krona
+        /// </summary>
+        SEK = 28,
+
+        /// <summary>
+        /// Switzerland franc
+        /// </summary>
+        CHF = 29,
+
+        /// <summary>
+        /// Thailand baht
+        /// </summary>
+        THB = 30,
+
+        /// <summary>
+        /// Turkey lira
+        /// </summary>
+        TRY = 31,
+
+        /// <summary>
+        /// United Kingdom pound
+        /// </summary>
+        GBP = 32,
+
+        /// <summary>
+        /// USA dollar
+        /// </summary>
+        USD = 33
+    }
+}
---
+++ CurrencyRates/Core/CurrencyRates.Core/Models/TimedCurrencyRatesModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CurrencyRates.Core.Models
+{
+    public class TimedCurrencyRatesModel
+    {
+        /// <summary>
+        /// List of currency in particular date range
+        /// </summary>
+        public List<LoaderCurrencyRatesModel> TimedRates { get; set; }
+
+        /// <summary>
+        /// Name of source from where downloaded current rates
+        /// </summary>
+        public string SourceName { get; set; }
+
+        /// <summary>
+        /// Версия сообщения Канареечный деплой
+        /// </summary>
+        public string Version { get; set; }
+    }
+}
